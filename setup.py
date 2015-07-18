eb5b5795e3a45974a38d056657a437626c2f6b35
Diogo Dutra <dutradda@gmail.com>
2015-07-18 03:09:12 -0300

    updating version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ elif sys.version_info >= (3, 0):
 
 setup(
     name='alquimia',
-    version='0.6.1',
+    version='0.7.1',
     author='Diogo Dutra',
     author_email='dutradda@gmail.com',
     description='An API to work with JSON schemas in SQLAlchemy',
